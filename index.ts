ad82fb65ff8eb0a8e28b1310251783d03e374f4f
Andrew Savelev <savelevcorr@gmail.com>
2020-04-03 20:59:41 +0300

    Algorithm to get max profit

--- index.ts
+++ index.ts
@@ -87,3 +87,22 @@ const charactersCounter = function (input: string): {[key: string]: number} {
     // Return result
     return caractersContainer;
 };
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. 
+ * You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices: number[]): number {
+    let result = 0;
+    
+   for (let i = 0; i < prices.length; i++) {
+       if (prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+        }
+   }
+    
+    return result;
+};
