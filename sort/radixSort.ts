804c08d7c905800a461b9ffec18a36acfa3a5c05
Andrew Savelev <savelevcorr@gmail.com>
2020-05-04 14:03:50 +0300

    Add the radix sort function

---
+++ sort/radixSort.ts
@@ -0,0 +1,34 @@
+import getMostDigitCount from '../utils/get-most-digit-count';
+import getDigit from '../utils/get-digit';
+
+/**
+ * The Radix Sort function. Full description is in the its body.
+ * @param nums {number[]}
+ */
+const radixSort = function (nums: number[]): number[] {
+
+    // figure out how many digits the largest number has
+    let maxDisgits: number =  getMostDigitCount(nums);
+    // will contans an array of array of numbers
+    let buckets: Array<Array<number>>;
+
+    // will contains a digit on the given position
+    let digit: number;
+
+    // loop from k = 0 up to this largest number of digits
+    for (let k = 0; k < maxDisgits; k++) {
+        // Create buckets for each digits (0 to 9)
+        buckets = Array.from({length: 10}, () => []);
+
+        // place each number in the corresponding bucket based on its k-th digit
+        for (let i = 0; i < nums.length; i++) {
+            digit = getDigit(nums[i], k);
+            buckets[digit].push(nums[i])
+        }
+
+        // replace our existing array with values in our buckets, starting from 0 and going up to 9
+        nums = [].concat(...buckets);
+    }
+
+    return nums;
+};
