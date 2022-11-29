32d33eb1d82f2fed1e6f713e5b5fd185b75df492
Andrei <andrei_savelev@epam.com>
2022-11-29 18:44:07 +0400

    feat: Implement basic DLL

---
+++ lists/doubly-linked-list/doubly-linked-list.ts
@@ -0,0 +1,97 @@
+import Node, { TNode } from "./node";
+
+interface IDoublyLinkedList<T> {
+    head: TNode<T>
+    tail: TNode<T>
+    length: number
+
+    /**
+     * @param value Adds one elelement to the end of a list and returns the new length of the list;
+     */
+    push(value: T): number
+
+    /**
+     * Removes the last element of a list and returns that element.
+     * This method changes the length of the list.
+     */
+    pop(): TNode<T>
+
+    // /**
+    //  * Remove the first element of a list and returns that element.
+    //  * This method changes the length of the list.
+    //  */
+    // shift(): TNode<T>
+}
+
+export default class SinglyLinkedList implements IDoublyLinkedList<string> {
+    head: TNode<string>
+    tail: TNode<string>
+    length: number;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value: string): number {
+        const node = new Node(value);
+
+        if (this.isListEmpty()) {
+            this.head = node;
+            this.tail = node;
+
+            this.incrementLength();
+
+            return this.length;
+        }
+
+        this.tail!.next = node;
+        node.prev = this.tail;
+
+        this.tail = node;
+
+        this.incrementLength();
+
+        return this.length;
+    }
+
+    pop() {
+        if (this.isListEmpty()) {
+            return null;
+        }
+
+        if (this.doesListContainOnlyOne()) {
+            this.head = null;
+            this.tail = null;
+
+            this.decrementLenght();
+
+            return null;
+        }
+
+        const toBeNext = this.tail!.prev;
+
+        this.tail = toBeNext;
+        this.tail!.next = null;
+        this.decrementLenght();
+
+        return toBeNext;
+    }
+
+    private incrementLength() {
+        this.length += 1;
+    }
+
+    private decrementLenght() {
+        this.length -= 1;
+    }
+
+    private isListEmpty() {
+        return this.length === 0;
+    }
+
+    private doesListContainOnlyOne() {
+        return this.length === 1;
+    }
+}
