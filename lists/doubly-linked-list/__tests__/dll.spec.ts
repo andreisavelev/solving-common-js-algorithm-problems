00c926eb6228661d205c49357272229692b70813
Andrei <andrei_savelev@epam.com>
2022-11-29 18:45:58 +0400

    feat: Implement basic DLL

---
+++ lists/doubly-linked-list/__tests__/dll.spec.ts
@@ -0,0 +1,47 @@
+import DoublyLinkedList from '../doubly-linked-list';
+
+describe('DLL', () => {
+    it('should have head, tail & length property', () => {
+        const dll = new DoublyLinkedList();
+
+        expect(dll).toHaveProperty('head');
+        expect(dll).toHaveProperty('tail');
+        expect(dll).toHaveProperty('length');
+    });
+
+    it('should push elements and the end of a list', () => {
+        const dll = new DoublyLinkedList();
+        const first = 'first';
+        const second = 'second';
+
+        dll.push(first);
+
+        expect(dll.tail?.value).toBe(first);
+        expect(dll.head?.value).toBe(first);
+        expect(dll.length).toBe(1);
+
+        dll.push(second);
+
+        expect(dll.head?.value).toBe(first);
+        expect(dll.tail?.value).toBe(second);
+        expect(dll.length).toBe(2);
+    });
+
+    it('should handle the pop method', () => {
+        const dll = new DoublyLinkedList();
+        const first = 'first';
+        const second = 'second';
+        const third = 'third';
+
+        dll.push(first);
+        dll.push(second);
+        dll.push(third);
+
+        dll.pop();
+
+        expect(dll.tail?.next).toBeNull();
+        expect(dll.tail?.value).toBe(second);
+        expect(dll.tail?.prev?.value).toBe(first);
+        expect(dll.length).toBe(2);
+    });
+})
---
+++ lists/doubly-linked-list/__tests__/node.spec.ts
@@ -0,0 +1,12 @@
+import Node from '../node';
+
+describe('Node', () => {
+    it('should create node as an instance of Node class', () => {
+        const nodeValue = 'Test';
+        const node = new Node(nodeValue);
+
+        expect(node.value).toBe(nodeValue);
+        expect(node.prev).toBe(null);
+        expect(node.next).toBe(null);
+    });
+})
