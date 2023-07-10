f84bb666d7d11a20be14d5df003d10f5acf37529
Andrei Savelev <andrei.savelev@yahoo.com>
2023-07-10 11:46:56 +0400

    feat: Stack

---
+++ stack/node.ts
@@ -0,0 +1,24 @@
+/**
+ * It stores the peace of data - value
+ * and a reference to the next node in the <b><i>next</i></b> field
+ * @author Andrei Savelev <savelevcorr@outlook.com>
+ */
+
+export type NodeEntity = INode<unknown> | null;
+
+export interface INode<T> {
+  next: NodeEntity;
+  value: T;
+}
+
+export class Node implements INode<unknown> {
+  public next: NodeEntity;
+  public value: unknown;
+
+  constructor(value: unknown) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export default Node;
