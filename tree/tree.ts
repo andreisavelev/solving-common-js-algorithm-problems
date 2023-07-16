01bc53c42b8ce73f07e32c3302c07128a3e00620
Andrei Savelev <savelevcorr@gmail.com>
2023-07-16 20:52:33 +0400

    feat: Tree (insert) (#20)
    * feat: Basic of Trees
    
    * feat: Tree (insert)
    
    * fix: Tree (insert)
    
    * fix(Tree): remove empty file
    
    * chore(actions): Re-run on push

---
+++ tree/tree.ts
@@ -0,0 +1,51 @@
+import type  { TTreeNodeEntity, } from "../shared/node";
+import { TreeNode } from "../shared/node";
+
+interface ITree<T> {
+  root: TTreeNodeEntity<T>;
+  insert(value: T): undefined
+}
+
+export class Tree implements ITree<number> {
+  root: TTreeNodeEntity<number>;
+
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value: number): undefined {
+    const node = new TreeNode(value) as TTreeNodeEntity<number>;
+
+    if (this.root === null) {
+      this.root = node;
+
+      return;
+    }
+
+    let currentNode = this.root;
+
+    while(true) {
+      if (value > currentNode.value) {
+        if (currentNode.right === null) {
+          currentNode.right = node;
+          
+          return;
+        } else {
+          currentNode = currentNode.right
+        }
+      } else if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = node;
+
+          return;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else if (value === currentNode.value) {
+        return;
+      } else {
+        throw new Error('Value should be only a number');
+      }
+    }
+  }
+}
